972d81dc559e05c544edeead665aec92a4674027
JUMAAAN <smsky910@naver.com>
2018-07-23 17:52:43 +0900

    자바 숫자찾기
    등록해봄

---
+++ Homework3.java
@@ -0,0 +1,28 @@
+package °´ÁöÇÁ;
+
+public class Homework3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int high=5;
+		int middle=3;
+		int low=7;
+		
+		if(high<middle&&high<low) {
+			System.out.println("The minimum number is " + high);
+		}
+		else if(middle<high&&middle<low) {
+			System.out.println("The minimum number is " + middle);
+		}
+		else {
+			System.out.println("The minimum number is " + low);
+		}
+		
+		
+		
+		
+		
+		
+	}
+
+}
